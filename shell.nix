ef250e07ea562f60c40525a4cb99aca9227744e2
Florian Schroedl <flo.schroedl@gmail.com>
2021-06-11 19:27:39 +0200

    Fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
-{ pkgs, nimpkgs, buildInputs }:
+{ pkgs, buildInputs }:
 
 pkgs.mkShell {
   shellHook = ''
   '';
   buildInputs = with pkgs; buildInputs ++ [
-    mozjpeg
+    nodejs
+    (pkgs.yarn.override { inherit nodejs; })
+
+    # Dependencies for mozjpeg
+    autoreconfHook  # Inject nixpkgs for mozjpeg build
+    automake autoconf libtool dpkg pkgconfig nasm libpng
   ];
 }
